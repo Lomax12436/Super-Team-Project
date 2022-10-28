51fd2839fa6eefb0931d7db000cd402a5bfdffd0
asimpkins17004 <asimpkins17004@yahoo.com>
2022-10-27 19:41:56 -0700

    Andrew-Project-branch

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var characterClass;
 var race;
 var gender;
-var allignment;
+var alignment;
 var name;
 var abilityChecks;
 var age;
 
 let abilityChecks = ["10", "11", "12", "13", "14", "15", "16"]
 let race = ["dragonborn", "dwarf", "elf", "gnome", "half-elf", "half-orc", "halfling", "human", "tiefling"]
-let age = []
+let gender = ["masculine", "feminine"]
+let characterClass = ["Barbarian", "Bard", "Claric", "Druid", "FIghter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
+let alignment = ["Chaotic Evil", "Chaotic Good", "Chaotic Nuetral", "Lawful Good", "Lawful Evil", "Lawful Nuetral", "Nuetral", "Nuetral Evil", "Nuetral Good"]
