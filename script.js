b9a842aa7adadb2c98d1fe69a59c2e8d08d56762
asimpkins17004 <asimpkins17004@yahoo.com>
2022-11-01 21:31:31 -0700

    Andrew-Project-branch

--- script.js
+++ script.js
@@ -1,13 +1,74 @@
-var characterClass;
-var race;
-var gender;
-var alignment;
-var name;
-var abilityChecks;
-var age;
+let characterName = document.getElementById("name");
+let charAge = [" Age: Young", "Age: Middle Aged", "Age: Elderly"];
+let charRace = [
+  "Race: Dragonborn",
+  "Race: Dwarf",
+  "Race: Elf",
+  "Race: Gnome",
+  "Race: Half-Elf",
+  "Race: Half-Orc",
+  "Race: Halfling",
+  "Race: Human",
+  "Race: Tiefling",
+];
+let charAlign = [
+  "Alignment: Chaotic Evil",
+  "Alignment: Chaotic Good",
+  "Alignment: Chaotic Neutral",
+  "Alignment: Lawful Evil",
+  "Alignment: Lawful Good",
+  "Alignment: Lawful Neutral",
+  "Alignment: Neutral",
+  "Alignment: Neutral Evil",
+  "Alignment: Neutral Good",
+];
+let charClass = [
+  "Class: Barbarian",
+  "Class: Bard",
+  "Class: Cleric",
+  "Class: Druid",
+  "Class: Fighter",
+  "Class: Monk",
+  "Class: Paladin",
+  "Class: Ranger",
+  "Class: Rogue",
+  "Class: Sorcerer",
+  "Class: Warlock",
+  "Class: Wizard",
+];
+let charGender = ["Gender: Male", "Gender: Female", "Gender: Non-Bionary"];
 
-let abilityChecks = ["10", "11", "12", "13", "14", "15", "16"]
-let race = ["dragonborn", "dwarf", "elf", "gnome", "half-elf", "half-orc", "halfling", "human", "tiefling"]
-let gender = ["masculine", "feminine"]
-let characterClass = ["Barbarian", "Bard", "Claric", "Druid", "FIghter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
-let alignment = ["Chaotic Evil", "Chaotic Good", "Chaotic Nuetral", "Lawful Good", "Lawful Evil", "Lawful Nuetral", "Nuetral", "Nuetral Evil", "Nuetral Good"]
+let character = {
+  name: "",
+  age: "",
+  gender: "",
+  race: "",
+  class: "",
+  align: "",
+};
+
+function getRandom() {
+  let randomAge = charAge[Math.floor(Math.random() * charAge.length)];
+  let randomRace = charRace[Math.floor(Math.random() * charRace.length)];
+  let randomAlign = charAlign[Math.floor(Math.random() * charAlign.length)];
+  let randomClass = charClass[Math.floor(Math.random() * charClass.length)];
+  let randomGender = charGender[Math.floor(Math.random() * charGender.length)];
+  character.age = randomAge;
+  character.race = randomRace;
+  character.align = randomAlign;
+  character.class = randomClass;
+  character.gender = randomGender;
+  console.log(character);
+  populateChar();
+}
+
+function populateChar() {
+  //document.getElementById("namePop").textContent = character.name;
+  document.getElementById("agePop").textContent = character.age;
+  document.getElementById("genderPop").textContent = character.gender;
+  document.getElementById("racePop").textContent = character.race;
+  document.getElementById("classPop").textContent = character.class;
+  document.getElementById("alignPop").textContent = character.align;
+}
+
+document.getElementById("randomize").addEventListener("click", getRandom);
